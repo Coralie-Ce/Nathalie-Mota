5ca51141e79a805447c36f2f256e7dfd34db8f63
Coralie Cellier <coralie.cellier@outlook.fr>
2025-02-12 15:04:58 +0100

    changements css

--- functions.php
+++ functions.php
@@ -15,3 +15,9 @@ function register_footer_menu() {
     register_nav_menu( 'footer-menu', __( 'Menu du pied de page', 'text-domain' ) );
 }
 add_action( 'after_setup_theme', 'register_footer_menu' );
+
+
+function enqueue_custom_styles() {
+    wp_enqueue_style('custom-theme-css', get_template_directory_uri() . '/css/style.css', array(), '1.0', 'all');
+}
+add_action('wp_enqueue_scripts', 'enqueue_custom_styles');
