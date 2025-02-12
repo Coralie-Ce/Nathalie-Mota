1043cbc2859fb24f8676090dcbe6f3e46d35a681
Coralie Cellier <coralie.cellier@outlook.fr>
2025-02-12 13:08:35 +0100

    Ajout des fichiers header.php et footer.php

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer>
+    <nav class="footer-menu">
+        <?php
+        // Affiche le menu de pied de page WordPress
+        wp_nav_menu([
+            'theme_location' => 'footer-menu', // Spécifie l'emplacement du menu dans le thème
+        ]);
+        ?>
+    </nav>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
---
+++ header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Site photographe freelance">
+    <meta name="keywords" content="Photo event">
+
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
+
+    <?php wp_head(); ?>
+
+</head>
+
+<body>
+
+<header class="site-header">
+    <div class="container">
+        <div class="logo">
+            <a href="<?php echo home_url(); ?>">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="<?php bloginfo('name'); ?>">
+            </a>
+        </div>
+
+    <nav class="main-nav">
+    <?php
+    wp_nav_menu(['theme_location' => 'main-menu',
+    ]);
+    ?>
+
+    // Inclure fichier template formulaire contact 
+    </nav>
+
+</header>
+
+</body>
+
+</html>
