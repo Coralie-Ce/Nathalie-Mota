8b2deb960ef9e3e0a4fe2f85448f8d6b1e0fb767
Coralie Cellier <coralie.cellier@outlook.fr>
2025-02-18 10:25:49 +0100

    modifs modale

--- template-parts/contact-modal.php
+++ template-parts/contact-modal.php
@@ -8,7 +8,7 @@
     <div class="modal-content">
         <span class="close">X</span>
         <!--insérer image contact-->
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-modal.png" alt="contact" />
+        <img class="img-formulaire" src="<?php echo get_template_directory_uri(); ?>/assets/contact-header.png" alt="contact" />
         <?php echo do_shortcode('[contact-form-7 id="805bb4e" title="Formulaire de contact"]'); ?>
     </div>
 </div>
