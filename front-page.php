c7e123871caba93cb9c331f9260ac924bd9fae9d
Coralie Cellier <coralie.cellier@outlook.fr>
2025-03-03 14:09:52 +0100

    ajout filtres page accueil

--- front-page.php
+++ front-page.php
@@ -7,6 +7,45 @@
 get_header();
 ?>
 
+<!-- Section filtres -->
+<div class="filter-bar">
+    <!-- Section catégorie-->
+    <select id="category-filter">
+            <option value="">Catégories</option>
+            <?php
+            $categories = get_terms(array('taxonomy' => 'categorie', 'hide_empty' => true));
+            foreach ($categories as $category) {
+                echo '<option value="' . esc_attr($category->slug) . '">' . esc_html($category->name) . '</option>';
+            }
+            ?>
+    </select>
+    <!-- Section format -->
+    <select id="format-filter">
+            <option value="">Formats</option>
+            <?php
+            $formats = get_terms(array('taxonomy' => 'format', 'hide_empty' => true));
+            foreach ($formats as $format) {
+                echo '<option value="' . esc_attr($format->slug) . '">' . esc_html($format->name) . '</option>';
+            }
+            ?>
+    </select>
+<!-- Section trier par -->
+    <select id="sort-filter">
+            <option value="date">Trier par</option>
+            <option value="recent">Les plus récents</option>
+            <option value="oldest">Les plus anciens</option>
+    </select>
+</div>
+
+
+
+
+<!-- Bloc photos -->
+
+<div class="container-photos">
+
+<?php include get_template_directory() . '/template-parts/photo-block.php'; ?>
+</div>
 
 <?php
 get_footer();
