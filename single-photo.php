9e5579d0588111d2f27c364b16e4975a3332e21b
Coralie Cellier <coralie.cellier@outlook.fr>
2025-03-08 10:46:35 +0100

    ouverture photo fullscreen et page depuis section photos apparentées

--- single-photo.php
+++ single-photo.php
@@ -145,8 +145,14 @@ if ($terms && !is_wp_error($terms)) {
         <div class="photo-item">
         <?php the_post_thumbnail('large'); ?>
         <div class="overlay">
+        <a href="<?php the_permalink(); ?>">
             <img src="<?php echo get_template_directory_uri(); ?>/assets/icon_eye.png" alt="Voir" class="icon-eye">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/icon_fullscreen.png" alt="Agrandir" class="icon-fullscreen">
+        </a>
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/icon_fullscreen.png" 
+             alt="Agrandir" 
+             class="icon-fullscreen" 
+             data-fullsrc="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>" 
+             data-title="<?php the_title(); ?>">
         </div>
     </div>
         <?php endwhile;
